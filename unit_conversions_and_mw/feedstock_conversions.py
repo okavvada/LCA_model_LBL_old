457553b3e46901e91785bfa79014d4cb58cbf921
Olga Kavvada <okavvada@gmail.com>
2018-01-12 16:30:32 -0800

    unit conversion files

---
+++ unit_conversions_and_mw/feedstock_conversions.py
@@ -0,0 +1,11 @@
+# kg cellulose per kg feedstock
+cellulose = {
+    "corn_stover": 0.35,
+    "sorghum":  2
+}
+
+# kg fermentable sugars per kg feedstock
+sugars = {
+    "corn_stover": 58.18,
+    "sorghum": 57.94
+}
