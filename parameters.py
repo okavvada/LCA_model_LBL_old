4d6f9f1a8dd554f5e99330632f47388d0cfed8d9
Olga Kavvada <okavvada@gmail.com>
2018-01-18 15:49:23 -0800

    no water

--- parameters.py
+++ parameters.py
@@ -11,9 +11,6 @@ fuel_aliases_path = "unit_conversions_and_mw/fuel_aliases.csv"
 co2_filepath = "io_tables/impact_vectors/co2_impact.csv"
 ch4_filepath = "io_tables/impact_vectors/ch4_impact.csv"
 n2o_filepath = "io_tables/impact_vectors/n2o_impact.csv"
-co2_water_filepath = "io_tables/impact_vectors_water/co2_impact.csv"
-ch4_water_filepath = "io_tables/impact_vectors_water/ch4_impact.csv"
-n2o_water_filepath = "io_tables/impact_vectors_water/n2o_impact.csv"
 
 #updated impact vectors with process.water.m3 from original io_tables/input_output_table_working.xls
 water_consumption_path = "io_tables/impact_vectors_water/water_consumption.csv"
